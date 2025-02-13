41b290d519075dc46ec7cfa0dff61b9623075905
Mamta Patil <mamtapatil44@gmail.com>
2025-02-12 20:30:18 -0500

    sum

--- multiply/index.js
+++ multiply/index.js
@@ -13,3 +13,20 @@ const multiply =(a)=>{
 }
 
 console.log("mul ",multiply(1)(2)(3)(4)(6)())
+
+
+const sum =(a)=>{
+   
+    return function(b){
+        if(b){
+        return sum(a+b)
+    }
+    else{
+        return a
+    }
+}
+
+
+}
+
+console.log("sum ",sum(1)(2)(3)(4)(6)())
